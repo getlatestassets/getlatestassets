218a6bb33ef161637ca5c15a16e46caff0e3f603
Andreas Heigl <andreas@heigl.org>
2018-04-12 07:36:41 +0200

    Add deployment

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+-e
+
+SERVER=misc.heigl.org
+mkdir -p ./tmp
+export VERSION="`git describe --tags | cut -d "-" -f 1`"
+TAG=`git describe --tags --abbrev=0`
+TAG="master"
+HASH=`git rev-parse $TAG`
+SED="sed -i "
+if [ `uname` = 'Darwin' ] ; then
+    SED="sed -i \"\""
+fi
+git archive --format=tar $TAG | tar x -C tmp
+cd tmp
+composer install --no-dev --prefer-dist
+
+tar czf $HASH.tgz .
+
+scp -r ./$HASH.tgz $SERVER:/var/www/com.getlatestassets.api/$HASH.tgz
+ssh $SERVER mkdir -p "/var/www/com.getlatestassets.api/$HASH"
+ssh $SERVER tar xz -f "/var/www/com.getlatestassets.api/$HASH.tgz" -C "/var/www/com.getlatestassets.api/$HASH" && echo "Extracted tgz"
+ssh $SERVER rm -rf "/var/www/com.getlatestassets.api/$HASH.tgz" && echo "Removed tgz"
+ssh $SERVER ln -sf "/var/www/com.getlatestassets.api/$HASH" "/var/www/com.getlatestassets.api/current" && echo "Set symbolic link"
+
+cd ..
+
+rm -rf ./tmp
